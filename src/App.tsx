ffb224bbee9bfa176bf5baa39e3963d7f9bee677
The Noah <thenoahbz@gmail.com>
2019-11-07 15:52:14 -0600

    Add hamburger menu on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {settings, history, storage, api} from "./lib";
 
 interface State{
   offline: boolean;
+  showHamburgerMenu: boolean;
 }
 
 class App extends React.Component<any, State>{
@@ -14,7 +15,8 @@ class App extends React.Component<any, State>{
     super(props);
 
     this.state = {
-      offline: false
+      offline: false,
+      showHamburgerMenu: false
     };
 
     this.loadSettings();
@@ -57,10 +59,11 @@ class App extends React.Component<any, State>{
             <div className="offline">Currently offline.</div>
           : null}
           <div className="navbar">
+            <button className="btn icon hamburger" onClick={() => this.setState({showHamburgerMenu: !this.state.showHamburgerMenu})}>&#xE700;</button>
             <Link to="/" className="logo"><svg viewBox="0 0 12.79 3.175" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><defs><linearGradient id="linearGradient10622" x1="227" x2="234" y1="180.5" y2="168.5" gradientTransform="matrix(.2646 0 0 .2646 7.144 -.7937)" gradientUnits="userSpaceOnUse" xlinkHref="#linearGradient1556"/><linearGradient id="linearGradient1556"><stop stopColor="#6f15af" offset="0"/><stop stopColor="#19a9d2" offset="1"/></linearGradient><linearGradient id="linearGradient10634" x1="73.72" x2="74.25" y1="46.99" y2="44.08" gradientTransform="translate(.09553 -.01933)" gradientUnits="userSpaceOnUse" xlinkHref="#linearGradient1556"/></defs><g transform="translate(-65.88 -43.79)"><path d="m67.2 43.79c-0.1466 0-0.2646 0.118-0.2646 0.2646s0.118 0.2646 0.2646 0.2646h0.2646c0.1466 0 0.2646 0.118 0.2646 0.2646s-0.118 0.2646-0.2646 0.2646h-0.5292c-0.1466 0-0.2646 0.118-0.2646 0.2646 0 0.1466 0.118 0.2646 0.2646 0.2646h0.5292c0.0042 0 0.0077-0.0024 0.01189-0.0026a0.926 0.926 0 0 0 0.0021 0.0021 0.926 0.926 0 0 0-5.16e-4 5.16e-4h0.2511c0.1466 0 0.2646 0.118 0.2646 0.2646s-0.118 0.2646-0.2646 0.2646h-1.058c-0.1466 0-0.2646 0.118-0.2646 0.2646s0.118 0.2646 0.2646 0.2646h0.2666c0.1454 0 0.2625 0.1171 0.2625 0.2625v0.0041c0 0.1454-0.1171 0.2625-0.2625 0.2625h1.178c0.0011 0 2e-3 -5.03e-4 0.0031-5.16e-4a0.926 0.926 0 0 0 0.05374-5.19e-4 0.926 0.926 0 0 0 0.8795-0.925l-0.0186-0.1855a0.926 0.926 0 0 0-0.2599-0.4754 0.926 0.926 0 0 0 0.2785-0.662l-0.0186-0.1855a0.926 0.926 0 0 0-0.9307-0.7405 0.926 0.926 0 0 0-5.16e-4 0h-0.2688zm-1.06 1.058c-0.1454 0-0.2625 0.1171-0.2625 0.2625v0.0041c0 0.1454 0.1171 0.2625 0.2625 0.2625h0.0041c0.1454 0 0.2625-0.1171 0.2625-0.2625v-0.0041c0-0.1454-0.1171-0.2625-0.2625-0.2625zm0.4268 1.608c-0.03156 0.01331-0.05993 0.03246-0.08372 0.05633 0.02373-0.02372 0.05229-0.04308 0.08372-0.05633z" fill="url(#linearGradient10622)"/><g fill="url(#linearGradient10634)" strokeWidth=".2646" aria-label="ZList"><path d="m70.05 46.6h1.722v0.3245h-2.191v-0.2977l1.65-2.385h-1.623v-0.3266h2.098v0.2915z"/><path d="m72.69 46.6h1.426v0.3245h-1.825v-3.01h0.3989z"/><path d="m74.93 46.93h-0.3824v-2.237h0.3824zm-0.4134-2.83q0-0.09302 0.05581-0.1571 0.05788-0.06408 0.1695-0.06408 0.1116 0 0.1695 0.06408 0.05788 0.06408 0.05788 0.1571 0 0.09302-0.05788 0.155t-0.1695 0.06201q-0.1116 0-0.1695-0.06201-0.05581-0.06201-0.05581-0.155z"/><path d="m76.84 46.33q0-0.155-0.1178-0.2398-0.1158-0.08682-0.4072-0.1488-0.2894-0.06201-0.461-0.1488-0.1695-0.08682-0.2522-0.2067-0.08062-0.1199-0.08062-0.2853 0-0.2749 0.2315-0.4651 0.2336-0.1902 0.5953-0.1902 0.3803 0 0.616 0.1964 0.2377 0.1964 0.2377 0.5023h-0.3845q0-0.1571-0.1344-0.2708-0.1323-0.1137-0.3349-0.1137-0.2088 0-0.3266 0.09095-0.1178 0.09095-0.1178 0.2377 0 0.1385 0.1096 0.2088t0.3948 0.1344q0.2873 0.06408 0.4651 0.153 0.1778 0.08888 0.2625 0.215 0.08682 0.124 0.08682 0.3039 0 0.2997-0.2398 0.4816-0.2398 0.1798-0.6222 0.1798-0.2687 0-0.4754-0.09508-0.2067-0.09508-0.3245-0.2646-0.1158-0.1716-0.1158-0.37h0.3824q0.01033 0.1922 0.153 0.3059 0.1447 0.1116 0.3803 0.1116 0.217 0 0.3473-0.08682 0.1323-0.08888 0.1323-0.2356z"/><path d="m78.24 44.15v0.5416h0.4175v0.2956h-0.4175v1.387q0 0.1344 0.05581 0.2026 0.05581 0.06615 0.1902 0.06615 0.06615 0 0.1819-0.02481v0.3101q-0.1509 0.04134-0.2935 0.04134-0.2563 0-0.3865-0.155-0.1302-0.155-0.1302-0.4403v-1.387h-0.4072v-0.2956h0.4072v-0.5416z"/></g></g></svg></Link>
             <span style={{flex: 1}}></span>
-            <nav>
-              <Link to="/" className="btn mobile-hide">Servers</Link>
+            <nav className={this.state.showHamburgerMenu ? "visible" : ""}>
+              <Link to="/" className="btn">Servers</Link>
               <Link to="/players" className="btn">Players</Link>
               <Link to="/help" className="btn">Help</Link>
               <Link to="/feedback" className="btn">Feedback</Link>
@@ -71,7 +74,7 @@ class App extends React.Component<any, State>{
             <Link to="/settings" className="btn icon">&#xE115;</Link>
             <Link to="/account" className="btn icon">&#xE77B;</Link>
           </div>
-          <div style={{flex: 1}}>
+          <div style={{flex: 1}} onClick={() => this.setState({showHamburgerMenu: false})}>
             <Switch>
               <Route path="/players" component={PlayerPage}/>
               <Route path="/privacy-policy" component={PrivacyPolicyPage}/>
