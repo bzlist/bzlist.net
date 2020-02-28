61d3daeb03e8d07e42ce2511184b6a422b638849
The Noah <thenoahbz@gmail.com>
2020-02-28 17:24:34 -0600

    Add more info to help page

--- src/components/pages/HelpPage.tsx
+++ src/components/pages/HelpPage.tsx
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom";
 import {version} from "../../../package.json";
 
 export const HelpPage = (): JSX.Element => (
+  /* tslint:disable:max-line-length */
   <div className="wrapper">
     <h1>Help</h1>
     <p>
@@ -12,6 +13,8 @@ export const HelpPage = (): JSX.Element => (
     </p>
     <h2>FAQ</h2>
     <p>
+      <span className="label">What happened to "real-time?"</span><br/>
+      Well, I'm getting there. It's a lot harder then it seems, and I will continue to work hard on it until it's finished.<br/><br/>
       <span className="label">How do I see details about a server?</span><br/>
       Click/tap on it and you will be brought to it's page. You can also manually go there by visiting bzlist.net/s/<b>HOSTNAME</b>/<b>PORT</b>.<br/><br/>
       <span className="label">How do I get back to the home page?</span><br/>
@@ -19,8 +22,9 @@ export const HelpPage = (): JSX.Element => (
       <span className="label">When chosing a team after clicking PLAY nothing happens.</span><br/>
       You must install <a href="https://github.com/The-Noah/bzflag-launcher" target="_blank" rel="noopener noreferrer">BZFlag Launcher</a>, however it is currently only supported on Windows.<br/><br/>
       <span className="label">Data doesn't seem to be updating.</span><br/>
-      If the API had to restart (due to an error or update) then refreshing the page should fix it. If you continue to experience issues contact me.
+      If the API had to restart (due to an error or update) then refreshing the page should fix it. Also data won't automatically update if you turned it off in <Link to="/settings/other">settings</Link>. If you continue to experience issues contact me.
     </p>
     <p><small>Release version <a href={`https://github.com/bzlist/bzlist.net/releases/tag/v${version}`} target="_blank" rel="noopener noreferrer">v{version}</a> See <a href={`https://github.com/bzlist/bzlist.net/compare/v${version}...master`} target="_blank" rel="noopener noreferrer">what's new</a>.</small></p>
   </div>
+  /* tslint:enable:max-line-length */
 );
