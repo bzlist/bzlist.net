ad8ece693a875f85c5c0cda2cc52a9d47ed2de50
The Noah <thenoahbz@gmail.com>
2020-06-17 15:25:17 -0500

    Fix incorrect player scores when viewing history

--- src/components/pages/ServerDetailsPage.tsx
+++ src/components/pages/ServerDetailsPage.tsx
@@ -310,7 +310,7 @@ export class ServerDetailsPage extends React.PureComponent<Props, State>{
                   </tr>
                 </thead>
                   <tbody>
-                    {this.state.server.players.map((player: Player) => <PlayerRow key={`${player.callsign}:${player.server}`} player={player} showServer={false}/>)}
+                    {this.state.server.players.map((player: Player) => <PlayerRow key={`${player.callsign}:${player.server}${this.state.server?.timestamp}`} player={player} showServer={false}/>)}
                   </tbody>
                 </table>
               </div>
